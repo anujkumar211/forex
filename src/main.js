01472e174b6e332729878acb7d5f61faec6b532d
anujkumar211 <45512077+anujkumar211@users.noreply.github.com>
2021-07-28 13:24:44 +0530

    save state after refresh

--- src/main.js
+++ src/main.js
@@ -5,18 +5,8 @@ import router from "@/router";
 import store from "@/store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/assets/css/style.css";
-// import { ValidationProvider } from 'vee-validate';
-
-// Vue.component('ValidationProvider', ValidationProvider);
-// import { ValidationProvider } from "vee-validate";
-
-// // Register it globally
-// // main.js or any entry file.
-// Vue.component("ValidationProvider", ValidationProvider);
-//Vue.use(VeeValidate, { useConstraintAttrs: false });
 import "@/plugins/vee-validate";
 Vue.config.productionTip = false;
-
 new Vue({
  router,
  store,
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
 Vue.use(Vuex);
+import createPersistedState from "vuex-persistedstate";
 import User from "./modules/user";
 const store = new Vuex.Store({
  modules: {
   User,
  },
+ plugins: [createPersistedState({ User })],
 });
 
 export default store;
